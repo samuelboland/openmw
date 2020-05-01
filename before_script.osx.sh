97de2f8c0fe699bbe4dc121b327d9863a9e10d6d
Sam Boland <sam.boland@appfolio.com>
2020-05-01 12:01:55 -0700

    Before Script

---
+++ before_script.osx.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -e
+
+export CXX=clang++
+export CC=clang
+
+DEPENDENCIES_ROOT="/Users/sam.boland/src/openmw-deps/"
+QT_PATH="/usr/local/Cellar/qt@5.5/5.5.1_1"
+CCACHE_EXECUTABLE=$(brew --prefix ccache)/bin/ccache
+mkdir build
+cd build
+
+cmake \
+-D CMAKE_PREFIX_PATH="$DEPENDENCIES_ROOT;$QT_PATH" \
+-D CMAKE_C_COMPILER_LAUNCHER="$CCACHE_EXECUTABLE" \
+-D CMAKE_CXX_COMPILER_LAUNCHER="$CCACHE_EXECUTABLE" \
+-D CMAKE_OSX_DEPLOYMENT_TARGET="10.9" \
+-D CMAKE_OSX_SYSROOT="macosx10.15" \
+-D CMAKE_BUILD_TYPE=Release \
+-D OPENMW_OSX_DEPLOYMENT=TRUE \
+-D DESIRED_QT_VERSION=5 \
+-D BUILD_ESMTOOL=FALSE \
+-D BUILD_MYGUI_PLUGIN=FALSE \
+-G"Unix Makefiles" \
+..
